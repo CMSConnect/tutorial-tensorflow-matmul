b99b0b9ca11548bf99c4b76fe063c2d1c23072bf
khurtado <khurtado@nd.edu>
2017-11-30 02:09:20 -0600

    Update tensorflow examples

--- tf_matmul.py
+++ tf_matmul.py
@@ -1,10 +1,12 @@
 # simple example to show the matrix multiplication with tensorflow
 
 import tensorflow as tf
+sess = tf.Session(config=tf.ConfigProto(log_device_placement=True))
 matrix1 = tf.constant([1.0,2.0,3.0,4.0], shape=[2, 2])
 matrix2 = tf.matrix_inverse(matrix1)
 product = tf.matmul(matrix1, matrix2)
-with tf.Session() as sess:
+# with tf.Session() as sess:
+with tf.Session(config=tf.ConfigProto(log_device_placement=True)) as sess:
     result = sess.run(product)
     print("result of matrix multiplication")
     print("===============================")
--- tf_matmul_wrapper_cvmfs.sh
+++ tf_matmul_wrapper_cvmfs.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+
 printf "Hostname:"; hostname
 echo "Operative system:"
 if [ -f /etc/redhat-release ]; then
@@ -8,9 +9,16 @@ else
   cat /etc/issue
 fi
 
-# Print nvidia-smi output, if present
-nvidia-smi 2>/dev/null
-echo
+
+# Setup CMSSW release with tensorflow support
+export VO_CMS_SW_DIR=/cvmfs/cms.cern.ch
+source $VO_CMS_SW_DIR/cmsset_default.sh
+release=CMSSW_9_4_0
+export SCRAM_ARCH=slc6_amd64_gcc630
+scramv1 project -f $release
+cd $release
+eval $(scramv1 runtime -sh)
+cd -
 
 # Check for tensorflow module
 python -c 'import tensorflow' > /dev/null 2>&1
